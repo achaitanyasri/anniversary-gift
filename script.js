7dc14581dec550dd306d5779e27a3ed9c2e9acfa
achaitanyasri <155837696+achaitanyasri@users.noreply.github.com>
2025-05-22 07:59:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll('.slide');
 
-function showSlide() {
-  slides.forEach((slide, i) => {
-    slide.classList.remove('active');
-    if (i === slideIndex) {
-      slide.classList.add('active');
-    }
-  });
-  slideIndex = (slideIndex + 1) % slides.length;
-}
-
-setInterval(showSlide, 3000);
-showSlide();
 
 // Scroll animation for timeline content
 const timelineContents = document.querySelectorAll('.timeline-content');
@@ -36,15 +22,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const startBtn = document.getElementById('start-btn');
   const slides = document.querySelectorAll('.slide');
   const music = document.querySelector('audio');
-  let currentSlide = 0;
+  let slideIndex = 0;
 
-  function startSlideshow() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
+  function showSlide() {
+    slides.forEach((slide, i) => {
+      slide.classList.remove('active');
+      if (i === slideIndex) {
+        slide.classList.add('active');
+      }
+    });
+    slideIndex = (slideIndex + 1) % slides.length;
   }
 
-  let slideshowInterval;
+  setInterval(showSlide, 3000);
+  showSlide();
 
   startBtn.addEventListener("click", function () {
     overlay.style.display = "none"; // Hide the overlay
